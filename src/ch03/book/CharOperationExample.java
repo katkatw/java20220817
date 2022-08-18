9e2a0b9478b654e7970f2dbc38b17da779fd3ffd
katkatw <kathlynwyqe@gmail.com>
2022-08-18 17:35:24 +0900

    오늘 마지막

--- src/ch03/book/CharOperationExample.java
+++ src/ch03/book/CharOperationExample.java
@@ -1,5 +1,16 @@
 package ch03.book;
 
 public class CharOperationExample {
-
+	public static void main(String[] args) {
+		char c1 = 'A' + 1;
+		char c2 = 'A';
+//		char c3 = c2 + 1;	// 컴파일 에러	
+		char c3 = (char) (c2 + 1);			//대체
+		System.out.println("c1: " + c1);
+		System.out.println("c2: " + c2);
+		//System.out.println("c3: " + c3);
+		System.out.println("c3: " + c3);	//대체
+		
+		
+	}
 }
