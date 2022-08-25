97de9e67d733134d56fda92632329b119dd68341
katkatw <kathlynwyqe@gmail.com>
2022-08-25 17:09:26 +0900

    eeeeeee

---
+++ src/ch05/book/exercise/dddddd.java
@@ -0,0 +1,15 @@
+package ch05.book.exercise;
+
+public class dddddd {
+	public static void main(String[] args) {
+		System.out.println(half(5));
+	}
+	
+	public static double half(double a) {
+		return a / 2;
+	}
+	
+	public static double half(int a) {
+		return a / 2;
+	}
+}
