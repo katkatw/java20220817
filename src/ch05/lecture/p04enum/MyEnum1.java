74b1c3303d48c36e95b69330b2335e4527ef3122
katkatw <kathlynwyqe@gmail.com>
2022-09-08 12:21:06 +0900

    5장 Enum

---
+++ src/ch05/lecture/p04enum/MyEnum1.java
@@ -0,0 +1,10 @@
+package ch05.lecture.p04enum;
+
+public enum MyEnum1 {// 클래스는 필드 생성자 메소드지만 enum은?
+	// 열거 상수 
+	// 그자체로 이미 (public static final)이다 인스턴스생성완료
+	CONSTANT_VALUE1,
+	CONSTANT_VALUE2,
+	CONSTANT_VALUE3, // 마지막 ,는 써도 되고 안써도 된다
+	
+}
