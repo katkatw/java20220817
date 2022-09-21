a7cf57f806cfdb2157df0991ff7178cfd11a10d7
katkatw <kathlynwyqe@gmail.com>
2022-09-21 17:10:10 +0900

    18장

---
+++ src/ch18/book/exercise/p7/AddLineNumberExample.java
@@ -0,0 +1,18 @@
+package ch18.book.exercise.p7;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class AddLineNumberExample {
+	public static void main(String[] args) {
+		// 정확한 경로로 수정하세요.
+		String name = "src/ch18/lecture/p18"
+		
+		// 작성 위치
+		InputStream is = System.in;
+		Reader reader = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(reader);
+	}
+}
