11b3c4650f996077eb1f36bd597421bc23b00045
katkatw <kathlynwyqe@gmail.com>
2022-09-22 16:27:26 +0900

    1818

--- src/ch18/book/exercise/Exercise10.java
+++ src/ch18/book/exercise/Exercise10.java
@@ -1,5 +1,10 @@
 package ch18.book.exercise;
 
 public class Exercise10 {
-
+	public static void main(String[] args) {
+		// Exercise 10 : 
+//						InputStream    OutputStream
+//						OutputStream   InputStream
+						
+	}
 }
